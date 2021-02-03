7ce4306a4ac15ae99da8a840017d09993f57f3b5
Fetije <fb34030@ubt-uni.net>
2021-02-03 13:14:40 +0100

    commit

--- bbevoting_project/settings.py
+++ bbevoting_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9%k^*0(qi*oktfa+#a3pp9g3eo-9vd(kws8+$t#lf2u&b#+u-*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
 
